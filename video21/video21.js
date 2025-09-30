5a1f15cf60a1046cff6b8b6df8740c9df4c42ac4
unknown <josephthien23@gmail.com>
2025-10-01 02:03:46 +0700

    console

--- video21/video21.js
+++ video21/video21.js
@@ -11,3 +11,10 @@ cu
 console.log("info");
 console.error("oops my error");
 console.warn("this is a warning");
+
+
+a = 18
+b = 'cm'
+c = 'Thien'
+console.log(c, 'has', 'dick', a, b);
+console.log('%cVãi Lồn!', 'color: red; font-weight: bold;');
