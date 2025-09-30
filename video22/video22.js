00e6d28bced973827a4ba5fd48cadd05187d26a7
unknown <josephthien23@gmail.com>
2025-10-01 02:12:39 +0700

    template strings

---
+++ video22/video22.js
@@ -0,0 +1,11 @@
+console.log("video22");
+
+let name = "Thien";
+let age = 18;
+let gender = 'male';
+let introduction =
+    `my name is ${name}, 
+i am ${age} years old 
+and my gender is ${gender}
+    `;
+console.log(introduction);
