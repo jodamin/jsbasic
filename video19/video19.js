6930c89fb7397c0299d90ccfea888c6059c00785
unknown <josephthien23@gmail.com>
2025-10-01 00:43:27 +0700

    data types

---
+++ video19/video19.js
@@ -0,0 +1,15 @@
+console.log("video19");
+
+//object
+
+const person = {
+    name: 'Thien',
+    age: 20,
+    height: '174cm',
+    address: 'BienHoa',
+}
+console.log(person);
+
+//array
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
