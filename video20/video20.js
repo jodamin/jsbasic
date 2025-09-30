9407a0105c6a977e7fb45016997111dc4b6bf852
unknown <josephthien23@gmail.com>
2025-10-01 01:08:17 +0700

    operator

---
+++ video20/video20.js
@@ -0,0 +1,12 @@
+console.log("video20");
+
+let a = 5;
+
+let e = ++a // tăng ngay lập tức
+// let e = a++ // tăng sau khi sử dụng (chỉ tăng cho biến a)
+console.log(e);
+
+//compare
+// == : so sánh giá trị
+// === : so sánh giá trị và kiểu dữ liệu (nên dùng)
+// tương tự, nên dùng !== thay vì !=
