74f19999182bdb2cee19457b0528273878f87e4b
unknown <josephthien23@gmail.com>
2025-10-01 02:23:32 +0700

    lab01

---
+++ lab01/video23.js
@@ -0,0 +1,15 @@
+console.log('video23 this is lab1')
+
+let fullName = 'Dang Minh Thien';
+let birthYear = 2006;
+let isStudent = true;
+
+const today = new Date();
+const currentYear = today.getFullYear();
+
+const myAge = currentYear - birthYear;
+console.log(
+    `Name: ${fullName}
+Age: ${birthYear}
+Student: ${isStudent}
+    `);
