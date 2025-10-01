d13c040a5ae39fa679c74561f2ae4abb9ca46b63
unknown <josephthien23@gmail.com>
2025-10-02 03:32:00 +0700

    lab01

--- lab01/lab01.js
+++ lab01/lab01.js
@@ -1,13 +1,19 @@
 console.log('video23 this is lab1')
 
+//khai báo biến
 let fullName = 'Dang Minh Thien';
 let birthYear = 2006;
 let isStudent = true;
 
+//lấy năm hiện tại
 const today = new Date();
 const currentYear = today.getFullYear();
+console.log('Current year:', currentYear, 'typeof:', typeof currentYear);
 
+//tính tuổi
 const myAge = currentYear - birthYear;
+
+//in ra thông tin: tên, năm sinh, có phải học sinh không
 console.log(
     `Name: ${fullName}
 Age: ${birthYear}
