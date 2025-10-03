de3a7c5666ca610f4efdd13594796e3eb56cf8f8
unknown <josephthien23@gmail.com>
2025-10-04 06:36:28 +0700

    for, while, dowhile loop

---
+++ chapter4/video28/video28.js
@@ -0,0 +1,24 @@
+console.log("video 28");
+
+//for
+let text = 'Dit me may!';
+
+for (let i = 0; i <= 5; i++) {
+    console.log(text, i, "lan");
+};
+
+console.log(text.length);
+
+//while
+let score = 10;
+while (score > 5) {
+    console.log(score);
+    score--;
+};
+
+//do...while
+let score1 = 10;
+do {
+    console.log(score1);
+    score1--;
+} while (score1 > 5);
