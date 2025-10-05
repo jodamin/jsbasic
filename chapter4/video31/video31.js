7a891d67eb79fa15fcbeff0d988db378391b3103
unknown <josephthien23@gmail.com>
2025-10-05 17:51:21 +0700

    arrow func, IIEF

---
+++ chapter4/video31/video31.js
@@ -0,0 +1,10 @@
+//arrow function
+let sum = () => {
+    console.log("hello dick");
+};
+sum();
+
+//IIFE: Immediately Invoked Function Expression
+(() => {
+    console.log("hello con cac");
+})();
