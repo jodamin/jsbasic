b913906bf6990ada1c9ef9531006fdd97bf2a35c
unknown <josephthien23@gmail.com>
2025-10-05 17:38:19 +0700

    function

---
+++ chapter4/video30/video30.js
@@ -0,0 +1,14 @@
+console.log("video 30");
+
+function greeting() {
+    console.log("anh thien say dien");
+};
+greeting();
+
+//sum A + B
+function sum(a, b) {
+    sum = a + b;
+    return sum;
+};
+console.log("sum =", sum(5, 10));
+
