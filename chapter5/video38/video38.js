6f8decef273c42fa81d58081e80c12ec1ab443f2
unknown <josephthien23@gmail.com>
2025-10-07 09:55:18 +0700

    modify array

---
+++ chapter5/video38/video38.js
@@ -0,0 +1,18 @@
+console.log("Video 38");
+
+let myArray = ['item1', 'item2', 'item3'];
+console.log(myArray[0]);
+
+let fruits = ['apple', 'banana', 'cherry'];
+fruits[1] = 'grape';
+
+//add item and remove item
+fruits.push('add new last item');
+fruits.unshift('add new first item');
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
