7dfabfe76869ca05e72bee4ec712cbacfd1bee3a
unknown <josephthien23@gmail.com>
2025-10-07 15:19:54 +0700

    object introduce

---
+++ chapter5/video42/video42.js
@@ -0,0 +1,30 @@
+console.log('video42')
+
+//object: key: value
+const mySelf = {
+    Name: 'Thien',
+    Age: 20,
+    Height: '174cm',
+    Class: 'University',
+    Address: {
+        Nation: 'Vietnam',
+        City: 'Bienhoa',
+        Street: 'Phamvanthuan'
+    }
+};
+
+const myFriend = {
+    Name: 'Holan',
+    Age: 21,
+    Height: '198cm',
+    Class: 'University',
+    Address: {
+        Nation: 'Australia',
+        City: 'Unolyen',
+        Street: 'Fiblu Bolien'
+    }
+};
+
+const relationship = [mySelf, myFriend]
+
+console.log('include:', relationship) 
