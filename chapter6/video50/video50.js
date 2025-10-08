6f39de453361f0068575e99a56def80a9a915d7c
unknown <josephthien23@gmail.com>
2025-10-08 10:24:13 +0700

    use event

---
+++ chapter6/video50/video50.js
@@ -0,0 +1,12 @@
+console.log('Use event')
+
+// const element = document.getElementById("title")
+// console.log(element)
+
+//---------------------------------------------
+const element1 = document.getElementById("myButton");
+console.log(element1);
+
+const handleClickButton = () => {
+    console.log('You already click this button, Ahh~~')
+};
