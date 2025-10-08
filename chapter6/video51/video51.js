59742be642a31bf14fd4890408d5975e2f75de30
unknown <josephthien23@gmail.com>
2025-10-08 10:40:49 +0700

    combine js with html

--- chapter6/video51/video51.js
+++ chapter6/video51/video51.js
@@ -6,8 +6,7 @@ const element = document.getElementById("button")
 //xu ly event ko can code o file html
 const useClick = () => {
     console.log('Okay, I am fine :<')
-}
-
+};
 element.addEventListener("click", useClick) //just use name func, not func()
 // console.log(element)
 
