9769a94d699f04a20cb0296abda2829f5b0064e9
unknown <josephthien23@gmail.com>
2025-10-08 09:13:32 +0700

    access html with dom

---
+++ chapter6/video49/video49.js
@@ -0,0 +1,8 @@
+const element = document.getElementById("username");
+console.log('element:', element)
+
+const myDiv = document.querySelector(".helloWorld")
+console.log('mydiv:', myDiv)
+
+const AllmyDiv = document.querySelectorAll(".helloWorld")
+console.log('myAlldiv:', AllmyDiv)
